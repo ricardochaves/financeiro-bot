b6be3f6c3db297dc3b40aeda44093d497836e96c
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2019-11-24 12:18:03 -0300

    change gunicorn config to .py

--- start-from-k8s.sh
+++ start-from-k8s.sh
@@ -5,8 +5,8 @@ python manage.py migrate
 if [[ ${DJANGO_BIND_ADDRESS+x} ]] && [[ ${DJANGO_BIND_PORT+x} ]];
 then
     echo "OK! Using custom ADRESSS $DJANGO_BIND_ADDRESS and PORT $DJANGO_BIND_PORT"
-    gunicorn -cfile:gunicorn_config.ini -b ${DJANGO_BIND_ADDRESS}:${DJANGO_BIND_PORT} base_site.wsgi
+    gunicorn -cpython:gunicorn_config -b ${DJANGO_BIND_ADDRESS}:${DJANGO_BIND_PORT} base_site.wsgi
 else
     echo "Using 0.0.0.0:8000"
-    gunicorn -cfile:gunicorn_config.ini -b 0.0.0.0:8000 base_site.wsgi
+    gunicorn -cpython:gunicorn_config -b 0.0.0.0:8000 base_site.wsgi
 fi
