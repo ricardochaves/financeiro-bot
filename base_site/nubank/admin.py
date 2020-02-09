839c79708c4a6ad5446eac67745d5c480539cbee
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2020-02-09 12:04:24 -0300

    added new action to nubank admin.py

--- base_site/nubank/admin.py
+++ base_site/nubank/admin.py
@@ -1,8 +1,16 @@
+from django.contrib import admin
+
 from base_site.nubank.models import NubankBankStatement
 from base_site.nubank.models import NubankCards
 from base_site.nubank.models import NubankItemSetup
 from base_site.nubank.models import NubankStatement
-from django.contrib import admin
+
+
+def make_processed(modeladmin, request, queryset):
+    queryset.update(is_processed=True)
+
+
+make_processed.short_description = "Mark selected stories as processed"
 
 
 class NubankCardsAdmin(admin.ModelAdmin):
@@ -13,6 +21,7 @@ class NubankStatementAdmin(admin.ModelAdmin):
     list_display = ("created_at", "is_processed", "amount", "description", "item_time", "title")
     list_filter = ("is_processed", "created_at", "item_time")
     ordering = ["-created_at"]
+    actions = [make_processed]
 
 
 class NubankItemSetupAdmin(admin.ModelAdmin):
@@ -24,6 +33,7 @@ class NubankBankStatementAdmin(admin.ModelAdmin):
     list_display = ("created_at", "is_processed", "title", "amount", "post_date")
     list_filter = ("is_processed", "created_at", "post_date")
     ordering = ["-created_at"]
+    actions = [make_processed]
 
 
 admin.site.register(NubankStatement, NubankStatementAdmin)
